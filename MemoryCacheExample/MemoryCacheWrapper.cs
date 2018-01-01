15f078867ff66691855ca0ced5723057b7439654
Minh Tam <minhtam.seta@gmail.com>
2018-01-01 11:18:34 +0700

    Memory Cache wrapper

---
+++ MemoryCacheExample/MemoryCacheWrapper.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+
+namespace MemoryCacheExample
+{
+    internal class MemoryCacheWrapper<T> where T : class
+    {
+        private readonly Func<string, T> _callBack;
+        private readonly MemoryCache _memoryCache;
+
+        public MemoryCacheWrapper(Func<string, T> factory, TimeSpan timeout, string name)
+        {
+            _memoryCache = MemoryCache.Default;
+            _callBack = factory;
+            TimeOut = timeout;
+            Name = name;
+        }
+
+        public virtual T this[string key]
+        {
+            get
+            {
+                T value;
+
+                if (_memoryCache.Contains(key))
+                    value = (T)_memoryCache[key];
+                else
+                {
+                    value = _callBack(key);
+                    var policy = new CacheItemPolicy
+                    {
+                        AbsoluteExpiration = new DateTimeOffset(DateTime.Now.Add(TimeOut))
+                    };
+                    _memoryCache.Set(key, value, policy);
+                }
+                return value;
+            }
+        }
+
+        public TimeSpan TimeOut { get; set; }
+
+        public string Name { get; set; }
+
+        public void Add(object objectToCache, string key)
+        {
+            _memoryCache.Add(key, objectToCache, DateTime.Now.AddMilliseconds(TimeOut.Ticks));
+        }
+
+        public T Get(string key)
+        {
+            return this[key];
+        }
+
+        public void Clear(string key)
+        {
+            _memoryCache.Remove(key);
+        }
+
+        public bool Exists(string key)
+        {
+            return _memoryCache.Get(key) != null;
+        }
+
+        public List<string> GetAll()
+        {
+            return _memoryCache.Select(keyValuePair => keyValuePair.Key).ToList();
+        }
+    }
+}
--- MemoryCacheExample/Program.cs
+++ MemoryCacheExample/Program.cs
@@ -9,6 +9,12 @@ namespace MemoryCacheExample
     {
         static void Main(string[] args)
         {
+            //Example1();
+            RunCustomCacheWrapper();
+        }
+
+        private static void Example1()
+        {
             Console.WriteLine("Cache simple object");
             Console.WriteLine($"First time running took {CacheSimpleObject()} miliseconds to finish ");
             Console.WriteLine($"Second time running took {CacheSimpleObject()} miliseconds to finish ");
@@ -44,6 +50,24 @@ namespace MemoryCacheExample
                 $"Example: {longTimeRunningObject.Name} - Long time running methods took {longTimeRunningObject.Seconds} to finish");
             stopWatch.Stop();
             return stopWatch.ElapsedMilliseconds;
+        }
+
+        private static void RunCustomCacheWrapper()
+        {
+            var rnd = new Random();
+            Func<string, LongTimeRunningObject> cacheMissCreateCallback = CacheMissCreateCallback;
+            var wrapper = new MemoryCacheWrapper<LongTimeRunningObject>(cacheMissCreateCallback, new TimeSpan(0, 0, 10), "CacheListOfLongTimeRunningObject");
+            wrapper.Add(new LongTimeRunningObject() { Name = "One", Seconds = rnd.Next(2, 4) }, "One");
+            wrapper.Add(new LongTimeRunningObject() { Name = "Two", Seconds = rnd.Next(2, 4) }, "Two");
+            Console.WriteLine($"Name: {wrapper["Two"].Name}, Seconds: {wrapper["Two"].Seconds}");
+            Console.WriteLine($"Name: {wrapper["Three"].Name}, Seconds: {wrapper["Three"].Seconds}");
+            Console.WriteLine($"Name: {wrapper["Four"].Name}, Seconds: {wrapper["Four"].Seconds}");
+        }
+
+        private static LongTimeRunningObject CacheMissCreateCallback(string key)
+        {
+            var rnd = new Random();
+            return new LongTimeRunningObject() { Name = "Name" + DateTime.Now.Ticks.ToString(), Seconds = rnd.Next(5, 10) };
         }
     }
 }
