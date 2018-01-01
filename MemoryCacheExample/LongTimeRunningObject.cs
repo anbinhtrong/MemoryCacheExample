f1d9540527d1ef8c32321025ca9f1b89f0803f28
Minh Tam <minhtam.seta@gmail.com>
2018-01-01 11:08:07 +0700

    Simple Cache

---
+++ MemoryCacheExample/LongTimeRunningObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MemoryCacheExample
+{
+    public class LongTimeRunningObject
+    {
+        public string Name { get; set; }
+
+        public int Seconds { get; set; }
+
+        public void LongTimeRunningMethod()
+        {
+            Seconds = new Random().Next(1, 2);
+            Thread.Sleep(new TimeSpan(0, 0, Seconds));
+        }
+    }
+}
---
+++ MemoryCacheExample/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.Caching;
+using System.Threading;
+
+namespace MemoryCacheExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cache simple object");
+            Console.WriteLine($"First time running took {CacheSimpleObject()} miliseconds to finish ");
+            Console.WriteLine($"Second time running took {CacheSimpleObject()} miliseconds to finish ");
+            Thread.Sleep(new TimeSpan(0, 0, new Random().Next(2, 4)));
+            Console.WriteLine($"Third time running took {CacheSimpleObject()} miliseconds to finish ");
+        }
+
+        private static long CacheSimpleObject()
+        {
+            string keyName = "LongTimeRunningObject";
+            LongTimeRunningObject longTimeRunningObject;
+            var stopWatch = Stopwatch.StartNew();
+
+            var cache = MemoryCache.Default;
+
+            if (cache.Contains(keyName))
+            {
+                longTimeRunningObject = (LongTimeRunningObject)cache.Get(keyName);
+            }
+            else
+            {
+                longTimeRunningObject = new LongTimeRunningObject();
+                longTimeRunningObject.Name = "CacheSimpleObject";
+                longTimeRunningObject.LongTimeRunningMethod();
+                var cacheItemPolicy = new CacheItemPolicy
+                {
+                    AbsoluteExpiration = new DateTimeOffset(DateTime.Now.AddSeconds(2))
+                };
+                cache.Add(keyName, longTimeRunningObject, cacheItemPolicy);
+            }
+
+            Console.WriteLine(
+                $"Example: {longTimeRunningObject.Name} - Long time running methods took {longTimeRunningObject.Seconds} to finish");
+            stopWatch.Stop();
+            return stopWatch.ElapsedMilliseconds;
+        }
+    }
+}
